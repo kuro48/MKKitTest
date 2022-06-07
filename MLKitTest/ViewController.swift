5919a553b44bc4e54fda2b952ef58bc646160795
RyunosukeKurokawa <kuroryu.basket@gmail.com>
2022-06-07 16:25:37 +0900

    Initial Commit

---
+++ MLKitTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MLKitTest
+//
+//  Created by 黒川龍之介 on 2022/06/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
